55d2ab9799bd3ffd074f9ad429b5889a6eed7ab4
joan.gonzalez <gonzalezpenyajoan@gmail.com>
2025-02-05 12:35:32 +0100

    Added cors to rest-api obligatorio

--- api-rest/feature/obligatorio/src/core/servers/rest-api.server.ts
+++ api-rest/feature/obligatorio/src/core/servers/rest-api.server.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from 'cors';
+import { ENV } from "#core/constants/env.constants.js";
 
 export const createRestApiServer = () => {
     const app = express();
     app.use(express.json());
+    app.use(
+        cors({
+            methods: ENV.CORS_METHODS,
+            origin: ENV.CORS_ORIGIN,
+        })
+    );
     return app;
 };
